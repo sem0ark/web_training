a8d2e13942640a3cae2678f0a1fa53af007a643a
sem0ark <arcadii.sem@gmail.com>
2022-03-04 09:14:16 +0300

    finished express crash course

--- node/express_crash_course/index.js
+++ node/express_crash_course/index.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const path = require('path');
-
-
+const exphbs = require('express-handlebars');
+const members = require('./members')
 const app = express();
 
-const logger = require('./middleware/logger')
+// const logger = require('./middleware/logger')
 
 // init middleware
 // app.use(logger);
 
+//Handlebars Middleware
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
+//Homepage route
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Member App',
+    members
+  })
+});
+
+// Set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
 
 //// don't need to do this
 // app.get('/', (req, res) => {
@@ -20,10 +37,7 @@ app.use(express.urlencoded({ extended: false }))
 // });
 
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Members API rout
+// Members API routes
 app.use('/api/members', require('./routs/api/members'));
 
 const PORT = process.env.PORT || 5000;
