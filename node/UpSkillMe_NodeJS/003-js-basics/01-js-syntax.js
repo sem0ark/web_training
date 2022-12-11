db6a9b383dca07036b2ef5f296260a1176c0384f
sem0ark <arcadii.sem@gmail.com>
2022-12-11 21:28:37 +0200

    Add 01-js-syntax.js

--- node/UpSkillMe_NodeJS/003-js-basics/01-js-syntax.js
+++ node/UpSkillMe_NodeJS/003-js-basics/01-js-syntax.js
@@ -15,5 +15,31 @@
 */
 
 function main() {
+  let x_1 = "This is a string"; // it is a string literal
+  let x_2 = 'This is also a string';
+  let x_3 = 'This is "also" a string';
+  // console.log(x_1.length);
+  // console.log(x_1.toLowerCase());
 
+  // --------------------------------------------------------
+  // numbers
+
+  let y_1 = 120; // it is a number
+  let y_2 = 120.2399691862937; // it is a float
+
+  // (-)Infinity -> "very large number"
+  // NaN -> Not a number, modtly got from an error
+  // Math -> module for computations
+  // console.log('Rounding', Math.round(y_2));
+  // console.log('It is quite random: ', Math.random());
+  
+  // --------------------------------------------------------
+  // Boolean -> true / false
+  // === -> is exactly equal?
+
+  // Further reading
+  // variables https://javascript.info/variables
+  // Data types https://javascript.info/types
 }
+
+main()
