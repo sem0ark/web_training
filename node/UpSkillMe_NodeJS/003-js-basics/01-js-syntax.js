fbf7d1d977f862eb2bf022d8a7065a48cb2331c1
sem0ark <arcadii.sem@gmail.com>
2022-12-11 18:06:22 +0200

    Added files from UpSkill.me

---
+++ node/UpSkillMe_NodeJS/003-js-basics/01-js-syntax.js
@@ -0,0 +1,19 @@
+/**
+ * Goal of the course: become a conversant
+ * 
+ * Versions of JS:
+ * - ECMAScript 5 / ES5 -> from 2012
+ * - ECMAStript 2015 / ES6
+ * - ECMAStript 2016, etc.
+ * 
+ * Transpilers, use new functionality + keeping compatability:
+ * - Babel -> babeljs.io
+ * 
+ * How to start? ->
+ * 1. WebBrowser + JS console -> development tools
+ * 2. Node console
+*/
+
+function main() {
+
+}
