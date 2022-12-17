b405846672b997d98dcb68296930da48b0326d37
sem0ark <arcadii.sem@gmail.com>
2022-12-17 14:49:04 +0200

    finished 01-callbacks theory

--- node/UpSkillMe_NodeJS/008-asynchronous/01-callbacks.js
+++ node/UpSkillMe_NodeJS/008-asynchronous/01-callbacks.js
@@ -15,9 +15,41 @@
  * 3. Non-blocking
  * 
  * JS has a single thread, but it has the mechanism for moving throug the remaining.
- * It is used with something like AJAX -> it is default to asynch
+ * It is used with something like AJAX -> it is default to asynch.
+ * 
+ * Asynch JS functions:
+ * 1. AJAX methods, XMLHttpRequest methods
+ * 2. setTimeout
+ * 3. 
+ * 
+ * How to work with asynch?:
+ * Callback pattern
+ * - asynch function runs in parallel with the main program
+ * - asynch function has as a papameter another function called callback function
+ * - callback function is run after the asuch function is over
+ * 
  * */
 
 const fs = require('fs');
 const key = fs.readFileSync('./008-asynchronous/key.txt', 'utf8');
 
+// setTimeout(() => { console.log('hello'); }, 500);
+// When setTimeout is called it is moved to the parallel branch of execution
+// After the passing the time of 500 ms, the callback function is run
+
+
+function main_example() {
+  console.log("Hi!");
+  setTimeout(() => {
+    console.log("Asynch result");
+  }, 1000);
+  console.log("Synch result");
+}
+
+function main_practice() {
+  let httpRequest = new XMLHttpRequest();
+}
+
+
+main_example()
+main_practice()
