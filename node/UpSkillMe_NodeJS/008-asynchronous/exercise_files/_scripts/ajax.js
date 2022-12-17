103974d2cdcae48cfe4484f1141b28a5c4a3dc27
sem0ark <arcadii.sem@gmail.com>
2022-12-17 15:19:08 +0200

    Complete callbacks

--- node/UpSkillMe_NodeJS/008-asynchronous/exercise_files/_scripts/ajax.js
+++ node/UpSkillMe_NodeJS/008-asynchronous/exercise_files/_scripts/ajax.js
@@ -1,16 +1,20 @@
-function get(url, success) {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', url);
-    httpRequest.onload = function() {
-        success(httpRequest.responseText);
+function get(url, success, fail) {
+  let httpRequest = new XMLHttpRequest(); // we use AJAX request to get the data
+  httpRequest.open('GET', url);
+  httpRequest.onload = function () { // we use the callback to process the data
+    if (httpRequest.status === 200) {
+      success(httpRequest.responseText);
+    } else {
+      fail(httpRequest.responseText);
     }
-    httpRequest.send();
+  }
+  httpRequest.send();
 };
 
 function successHandler(data) {
-    const dataObj = JSON.parse(data);
-    const weatherDiv = document.querySelector('#weather');
-    const weatherFragment = `
+  const dataObj = JSON.parse(data);
+  const weatherDiv = document.querySelector('#weather');
+  const weatherFragment = `
         <h1>Weather</h1>
         <h2 class="top">
         <img
@@ -24,16 +28,31 @@ function successHandler(data) {
         <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
         </p>
     `
-    weatherDiv.innerHTML = weatherFragment;
-    weatherDiv.classList.remove('hidden');
+  weatherDiv.innerHTML = weatherFragment;
+  weatherDiv.classList.remove('hidden');
+}
+
+function failHandler(status) {
+  console.log('There is an error: ', status);
+  const weatherDiv = document.querySelector('#weather');
+  weatherDiv.classList.remove('hidden');
 }
 
 function tempToF(kelvin) {
-    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+  return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = ''; // ADD YOUR API KEY BETWEEN THE QUOTES
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
-    get(url, successHandler);
+
+document.addEventListener('DOMContentLoaded', function () {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", './key.txt', false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + rawFile.responseText;
+        get(url, successHandler);
+      }
+    }
+  }
+  rawFile.send(null);
 });
