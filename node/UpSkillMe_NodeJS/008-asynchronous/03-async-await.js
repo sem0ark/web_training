9d6c60b0356cac08197d8616ffd8212d3b156f6f
sem0ark <arcadii.sem@gmail.com>
2022-12-17 16:37:04 +0200

    Add 03-async-await.js

---
+++ node/UpSkillMe_NodeJS/008-asynchronous/03-async-await.js
@@ -0,0 +1,15 @@
+
+/**
+ * Notes on the course
+ * UpSkillMe_Node.js. JavaScript Basics
+ * Completed by Arkadii Semenov on 2022.12.17
+ * 
+ * All in all async/await is just a syntactic sugar, so it is working in the same way as promises!
+ * 
+ * We can use them by defining function that returns a Promise with *async*
+ *  and later use *await* to stop teh execution flow and get data from the asynchronous function.
+ * 
+ * Further reading:
+ * 1. Asynch/await: https://javascript.info/async-await
+ * 
+ * */
