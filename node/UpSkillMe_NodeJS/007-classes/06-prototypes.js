2b9dcb2402bedca4a9326602a510cac87fc56238
sem0ark <arcadii.sem@gmail.com>
2022-12-17 10:03:29 +0200

    Add information about prototypes from flowers tutorial

--- node/UpSkillMe_NodeJS/007-classes/06-prototypes.js
+++ node/UpSkillMe_NodeJS/007-classes/06-prototypes.js
@@ -10,3 +10,121 @@
  * 
  * */
 
+dataObject = {
+  itemname: '',
+  category: '',
+  vasetype: '',
+  pottype: '',
+  qty: '',
+}
+
+function main_flowers() {
+  if (dataObject.category === 'arrangement') {
+    // newItem = {
+    //   type: 'floral',
+    //   storage: 'cool',
+    //   name: dataObject.itemname,
+    //   vase: dataObject.vasetype,
+    //   quantity: dataObject.qty,
+    //   logItem: function() {
+    //     console.log('%c' + this.name, 'font-weight:bold');
+    //     for(let prop in this) {
+    //       console.log(' ', prop, ': ', this[prop]);
+    //     }
+    //   }
+    // }
+    newItem = new Arrangement(dataObject.itemname, dataObject.vasetype, dataObject.qty);
+  } else if (dataObject.category === 'live') {
+    // newItem = {
+    //   type: 'floral',
+    //   storage: 'warm',
+    //   name: dataObject.itemname,
+    //   pot: dataObject.pottype,
+    //   quantity: dataObject.qty,
+    //   logItem: function() {
+    //     console.log('%c' + this.name, 'font-weight:bold');
+    //     for(let prop in this) {
+    //       console.log(' ', prop, ': ', this[prop]);
+    //     }
+    //   }
+    // }
+    newItem = new Live(dataObject.itemname, dataObject.pottype, dataObject.qty);
+  } else if (dataObject.category === 'bouquet') {
+    // newItem = {
+    //   type: 'floral',
+    //   storage: 'warm',
+    //   name: dataObject.itemname,
+    //   vase: dataObject.vasetype,
+    //   flowers: {},
+    //   logItem: function() {
+    //     console.log('%c' + this.name, 'font-weight:bold');
+    //     for(let prop in this) {
+    //       console.log(' ', prop, ': ', this[prop]);
+    //     }
+    //   }
+    // }
+    newItem = new Bouquet(dataObject.itemname, dataObject.vasetype);
+    if (dataObject['color' + stemType] !== '---')
+      newItem.flowers.addStem(key, dataObject[item], dataObject['color' + stemType]);
+    else newItem.flowers.addStem(key, dataObject[item]);
+  }
+
+  function Arrangement(name, vase, quality = 1) {
+    this.type = 'floral';
+    this.storage = 'cool';
+    this.vase = vase;
+    this.name = name;
+    this.quantity = quantity;
+    this.logItem = function () {
+      console.log('%c' + this.name, 'font-weight:bold');
+      for (let prop in this) {
+        console.log(' ', prop, ': ', this[prop]);
+      }
+    }
+  }
+
+  function Live(name, pot, quality = 1) {
+    this.type = 'floral';
+    this.storage = 'cool';
+    this.pot = pot;
+    this.name = name;
+    this.quantity = quantity;
+    this.logItem = function () {
+      console.log('%c' + this.name, 'font-weight:bold');
+      for (let prop in this) {
+        console.log(' ', prop, ': ', this[prop]);
+      }
+    }
+  }
+
+  function Bouquet(name, vase, quality = 1) {
+    this.type = 'floral';
+    this.storage = 'cool';
+    this.vase = vase;
+    this.name = name;
+    this.logItem = function () {
+      console.log('%c' + this.name, 'font-weight:bold');
+      for (let prop in this) {
+        console.log(' ', prop, ': ', this[prop]);
+      }
+    }
+    this.flowers = {
+      addStem: function (name, quantity = 1, color = 'Default') {
+        this[name] = new Flower(quantity, color);
+      }
+    }
+  }
+
+  function Flower(quantity, color) {
+    this[color] = quantity;
+    this.logItem = function () {
+      console.log('%c' + this.name, 'font-weight:bold');
+      for (let prop in this) {
+        console.log(' ', prop, ': ', this[prop]);
+      }
+    }
+  }
+}
+
+// If we create similar, but a lot of objects, we can use constructors and even create cutom ones
+// Name constructros with a capital letter.
