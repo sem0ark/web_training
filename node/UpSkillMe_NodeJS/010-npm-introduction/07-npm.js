5fe0205f164c2124437391942c92bac4174bf0a7
sem0ark <arcadii.sem@gmail.com>
2022-12-20 16:46:08 +0200

    Add 010-npm, add exercise files

---
+++ node/UpSkillMe_NodeJS/010-npm-introduction/07-npm.js
@@ -0,0 +1,29 @@
+/**
+ * Notes on the course
+ * UpSkillMe_Node.js. JavaScript Basics
+ * Completed by Arkadii Semenov on 2022.12.20
+ * 
+ * NPM == Node Package Manager
+ * It is used to manage and install node mosules.
+ * npm is preinstalled with Node.js.
+ * 
+ * package.json file
+ * - the main file for the npm project.
+ * - it contains the list of dependencies.
+ * - it contains information about who created the project and when.
+ * 
+ * The site of npm contains more than 1000 packages, so we can you a lot of precreated functionality.
+ * 
+ * Commands:
+ * 1. npm init - initialize package.json file in the repository
+ *                Be careful with the name of the package, if you would post it on npm, then it would be seen to everyone.
+ * 2. npm install [-g] [package name]
+ *      - install the specifeid package with npm
+ *      - we can install package locally (only in the project folder)
+ *        or globally (to the system folder, so you can access it)
+ *    Example: npm install express
+ * 
+ * 
+ * 
+ * */
+
