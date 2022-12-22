19a4b36811d8427aa1dfc55fe37a4ead882c7e14
sem0ark <arcadii.sem@gmail.com>
2022-12-22 15:47:52 +0200

    Add mymodule

---
+++ node/UpSkillMe_NodeJS/011-node-essentials/03-my-module.js
@@ -0,0 +1,8 @@
+/**
+ * Notes on the course
+ * UpSkillMe_Node.js. Node.js: Essential Training
+ * Completed by Arkadii Semenov on 2022.12.21
+ * 
+ * We can use module.exports to export any functionality form the file.
+ * */
+
