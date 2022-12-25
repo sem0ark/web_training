21b632960873f40d2d34ab9202cf893e8ccb3bb7
sem0ark <arcadii.sem@gmail.com>
2022-12-25 10:13:12 +0200

    Complete collbacks

--- node/UpSkillMe_NodeJS/012-advanced-node/01-callbacks.js
+++ node/UpSkillMe_NodeJS/012-advanced-node/01-callbacks.js
@@ -12,16 +12,37 @@
 
 function main_1() {
   function hideString(str, done) {
-    process.nextTick(() => {
+    process.nextTick(() => { // Makes Node ot run the command on the next loop
       done(str.replace(/[[a-zA-Z]]/g, 'X'));
     });
   }
 
   hideString("Hello World", (hidden) => { // we use a callback here
-    console.log(hidden);
+    console.log(hidden); // Our string is logged on the next loop
   });
   console.log('end');
 }
 
+function main_2() {
+  function delay(seconds, callback) {
+    setTimeout(callback, seconds * 1000);
+  }
 
-main_1();
+  console.log('starting delays');
+  delay(2, () => {
+    console.log('two seconds');
+    delay(1, () => {
+      console.log('three seconds');
+      delay(1, () => {
+        console.log('four seconds');
+      });
+    });
+  });
+
+  // Here we create a sequential running with callbacks
+  // !! But it is an example of pretty nasty and unreadable code
+  // It is also an anti-pattern called *Callback hell* or *Pyramid of doom*.
+}
+
+// main_1();
+main_2();
