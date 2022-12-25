7b85d6cb2500a6cbbc2d7f7fa7a6075294984800
sem0ark <arcadii.sem@gmail.com>
2022-12-25 11:44:18 +0200

    Complete promises

--- node/UpSkillMe_NodeJS/012-advanced-node/02-promises.js
+++ node/UpSkillMe_NodeJS/012-advanced-node/02-promises.js
@@ -6,6 +6,10 @@
  * By the use of callbacks we can quickly run into an anti-pattern called Calback hell,
  * so to settle things down we can use Promises.
  * 
+ * Further reading:
+ * The Node.js Event Loop, Timers, process.nextTick()
+ *    https://nodejs.org/en/docs/guides/event-loop-timers-and-nexttick/
+ * 
  * */
 
 function main_resolving() {
@@ -65,6 +69,8 @@ function main_rejecting() {
 }
 
 function main_promisifying() {
+  const { promisify } = require('util');
+
   // JS creates a simple way to convert the function with callbacks
   // to a Promise, so we can handle them in a more readable way
 
@@ -81,5 +87,86 @@ function main_promisifying() {
     }
   };
 
-  
+  const promiseDelay = promisify(delay);
+  promiseDelay(2)
+    .then(console.log)
+    .catch(error => console.log(`error: ${error.message}`));
 }
+
+function main_promisifying_example() {
+  // Here we can see an example of using promisify the function from fs module
+
+  const { promisify } = require('util');
+  const fs = require('fs');
+
+  const writeFile = promisify(fs.writeFile);
+
+  writeFile('dummy.txt', 'This is a sample')
+    .then(console.log('file was created'))
+    .catch(error => console.log(error.message));
+}
+
+// main_promisifying_example();
+
+function main_callback_solution() {
+  const fs = require('fs');
+  const { promisify } = require('util');
+
+  const writeFile = promisify(fs.writeFile);
+  const unlink = promisify(fs.unlink);
+  const delay = (seconds) => new Promise((resolve) =>{
+    setTimeout(resolve, seconds*1000);
+  });
+
+  const beep = () => process.stdout.write("\x07");
+
+  const doStuffSequentiallyCB = () => {
+      console.log('starting');
+      setTimeout(() => {
+          console.log('waiting');
+          setTimeout(() => {
+              console.log('waiting some more');
+              fs.writeFile('file.txt', 'Sample File...', error => {
+                  if (error) {
+                      console.error(error);
+                  } else {
+                      beep();
+                      console.log('file.txt created')
+                      setTimeout(() => {
+                          beep();
+                          fs.unlink('file.txt', error => {
+                              if (error) {
+                                  console.error(error);
+                              } else {
+                                  console.log('file.txt removed');
+                                  console.log('sequential execution complete');
+                              }
+                          })
+                      }, 3000)
+                  }
+              });
+          }, 2000)
+      }, 1000)
+  } // Big and scary example of the pyramid of doom
+
+  const doStuffSequentiallyPR = () => Promise.resolve()
+    .then(() => console.log('starting'))
+    .then(() => delay(1))
+    .then(() => 'waiting')
+    .then(console.log)
+    .then(() => delay(2))
+    .then(() => writeFile('dummy.txt', 'Sample file'))
+    .then(beep)
+    .then(() => 'dummy.txt was created')
+    .then(console.log)
+    .then(() => delay(3))
+    .then(() => unlink('dummy.txt'))
+    .then(beep)
+    .then(() => 'dummy.txt was deleted')
+    .then(console.log)
+    .catch(console.error);
+  // Here we managed to specify the same code with the use of Promises, so it is much cleaner!
+  doStuffSequentiallyPR();
+}
+
+main_callback_solution();
