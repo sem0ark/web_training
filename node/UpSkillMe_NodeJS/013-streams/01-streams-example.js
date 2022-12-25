fc4f49e8412dee19e0165046c81f006241699622
sem0ark <arcadii.sem@gmail.com>
2022-12-25 16:41:14 +0200

    Add Readable stream

---
+++ node/UpSkillMe_NodeJS/013-streams/01-streams-example.js
@@ -0,0 +1,42 @@
+/**
+ * Notes on the course
+ * UpSkillMe_Node.js. Node.js: Essential Training
+ * Completed by Arkadii Semenov on 2022.12.24
+ * 
+ * Streams is the way to pas data in doable chuncks,
+ *   which can enchance efficiency of RAM usage and shrink
+ *   the time needed to get/watch data.
+ * 
+ * node --trace_gc filename.js
+ *  this comand tells the Node to show the behaviour of the garbage collector
+ *  from the current session.
+ * 
+ * There are two types:
+ * 1. Mark-sweep - actually stops the NOde process to clear a lot of garbage data
+ * 2. Scavege - clears less data and keep Node running
+ * 
+ * */
+
+const fs = require('fs');
+const http = require('http');
+
+const file = './powder-day.mp4';
+
+http.createServer((req, res) => {
+  // fs.readFile(file, (err, data) => {
+  //   if (err) {
+  //     console.log(`hmm: ${error.message}`);
+  //   }
+  //   res.end(data); // Here we pass all available data to the user -> unefficient and consumes a lot of resourses
+  // });
+
+  res.writeHeader(200, {'Content-Type': 'video/mp4'});
+  fs.createReadStream(file)
+    .pipe(res)
+    .on('error', console.error);
+
+  // By the usage of streams we can greately stop the usage of memory
+  // so our app would be more efficient
+
+  // We are sending the data bit by bit
+}).listen(3000, () => console.log('buffer - http://localhost:3000'));
---
+++ node/UpSkillMe_NodeJS/013-streams/02-read-stream.js
@@ -0,0 +1,100 @@
+/**
+ * Notes on the course
+ * UpSkillMe_Node.js. Node.js: Essential Training
+ * Completed by Arkadii Semenov on 2022.12.24
+ * 
+ * Streams is the way to pas data in doable chuncks,
+ *   which can enchance efficiency of RAM usage and shrink
+ *   the time needed to get/watch data.
+ * 
+ * Readable stream reads data from the file and passes it chunk by chunk.
+ * 
+ * REadable streams are used everywhere:
+ * - files
+ * - internet
+ * - zipping
+ * - etc.
+ * 
+ * */
+
+const { Readable } = require('stream');
+
+const fs = require('fs');
+
+
+const peaks = [
+  "Tallac",
+  "Ralston",
+  "Rubicon",
+  "Twin Peaks",
+  "Castle Peak",
+  "Rose",
+  "Freel Peak"
+];
+
+
+class StreamFromArray extends Readable {
+  constructor(arr) {
+    // super({ encoding: 'utf-8' });
+    super({ objectMode: true});
+    // without passing the encoding we would use the binary mode for reading from the stream
+    // by passing objectMode:true we would be able to pass any type of JS object
+    this.array = arr;
+    this.index = 0;
+  }
+
+  _read() {
+    if (this.index < this.array.length) {
+      // const chunk = this.array[this.index]; // if you send as a string
+      const chunk = {
+        data: this.array[this.index],
+        index: this.index,
+      }
+      this.push(chunk); // actually sending the data
+      this.index++;
+    } else {
+      this.push(null); // sending a signal that the reading is over
+    }
+  }
+}
+
+function main_read() {
+  const stream = new StreamFromArray(peaks);
+
+  stream.on('data', (chunk) => {
+    console.log(chunk);
+  }); // the event os triggered when the stream sends the data
+
+  stream.on('end', (chunk) => {
+    console.log('Done!');
+  });
+}
+
+// main_read();
+
+function main_using() {
+  const readStream = fs.createReadStream('./powder-day.mp4');
+  readStream.on('data', (chunk) => {
+    console.log('Reading little chunk\n Of length: ', chunk.length);
+  });
+  // Because we are reading the data non-stop, we can say that hte stream is in flowing mode
+  // By invoking the method pause, we can make it asking for the next bit of data
+
+  readStream.on('end', () => console.log('Read stream finished'));
+
+  readStream.on('error', (error) => {
+    console.log('An error has occured.');
+    console.error(error);
+  })
+
+  readStream.pause(); // Here we are stopping teh stream
+  process.stdin.on('data', (chunk) => {
+    const text = chunk.toString().trim();
+    if (text === 'finish') {
+      readStream.resume();
+    }
+    readStream.read();
+  });
+}
+
+// main_using();
