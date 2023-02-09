42a2b085759a325a0f9d0e25a0cf27889d94ab01
sem0ark <arcadii.sem@gmail.com>
2023-02-09 13:11:38 +0100

    Add notes on Mongoose, Complete MongoDB

--- node/UpSkillMe_NodeJS/018-intro-to-db/shopper/server/models/mongoose/User.js
+++ node/UpSkillMe_NodeJS/018-intro-to-db/shopper/server/models/mongoose/User.js
@@ -1,38 +1,59 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const UserSchema = mongoose.Schema({
-  email: {
-    // Trim and lowercase
-    type: String, required: true, index: { unique: true }, lowercase: true, trim: true,
+const UserSchema = mongoose.Schema(
+  {
+    email: {
+      // Trim and lowercase
+      type: String,
+      required: true,
+      index: { unique: true },
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
-  password: {
-    type: String, required: true, trim: true,
-  },
-}, { timestamps: true });
+  {
+    timestamps: true,
+  }
+);
 
 async function generateHash(password) {
   const COST = 12;
   return bcrypt.hash(password, COST);
 }
 
-UserSchema.pre('save', function preSave(next) {
+UserSchema.pre("save", function preSave(next) {
+  // here is an example of hook
+  // we use it to update the data before the saving
   const user = this;
 
   // Only create a new password hash if the field was updated
-  if(user.isModified('password')) {
-    return generateHash(user.password).then(hash => {
-      user.password = hash;
-      return next();
-    }).catch(error => {
-      return next(error);
-    });
+  if (user.isModified("password")) {
+    return generateHash(user.password)
+      // we actually store the hash of the password
+      // so the the data with passwords wouldn't be compromised
+      .then((hash) => {
+        user.password = hash;
+        return next();
+      })
+      .catch((error) => {
+        return next(error);
+      });
   }
   return next();
 });
 
-UserSchema.methods.comparePassword = async function comparePassword(candidatePassword) {
+UserSchema.methods.comparePassword = async function comparePassword(
+  candidatePassword
+) {
   return bcrypt.compare(candidatePassword, this.password);
+  // we use the compare function to check
+  // if the password's really the same as the entered one
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
