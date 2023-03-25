60c3667028062e9e79d14c7c898af658419519b2
sem0ark <arcadii.sem@gmail.com>
2023-03-25 12:14:16 +0100

    Add aditional features of ts

---
+++ node/UpSkillMe_NodeJS/022-typescript/03 Additional features/01 Types.ts
@@ -0,0 +1,5 @@
+// Notes on the course Typescript for Node.js from EPAM UpSkillMe Node.js program
+// Completed by Arkadii Semenov on 2023-03-25
+
+// Further reading:
+// - [Discriminated Unions](https://basarat.gitbook.io/typescript/type-system/discriminated-unions#discriminated-union)
