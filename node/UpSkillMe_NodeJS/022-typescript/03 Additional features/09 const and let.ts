dfbea223689268ad638f6d461626488884805baf
sem0ark <arcadii.sem@gmail.com>
2023-03-25 14:31:58 +0100

    Add features 7

---
+++ node/UpSkillMe_NodeJS/022-typescript/03 Additional features/09 const and let.ts
@@ -0,0 +1,8 @@
+// Notes on the course Typescript for Node.js from EPAM UpSkillMe Node.js program
+// Completed by Arkadii Semenov on 2023-03-25
+
+(() => {
+  let text = "text text text";
+  const text2 = "text text text";
+  // text2 = "another text"; -> error
+})();
---
+++ node/UpSkillMe_NodeJS/022-typescript/03 Additional features/10 assignment assertions.ts
@@ -0,0 +1,8 @@
+// Notes on the course Typescript for Node.js from EPAM UpSkillMe Node.js program
+// Completed by Arkadii Semenov on 2023-03-25
+
+(() => {
+  let num: number;
+
+  function apply() {}
+})();
