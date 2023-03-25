446ea8d823bfca6f024425de63167dda10158c63
sem0ark <arcadii.sem@gmail.com>
2023-03-25 14:27:37 +0100

    Add features 6

---
+++ node/UpSkillMe_NodeJS/022-typescript/03 Additional features/08 Catch clause.ts
@@ -0,0 +1,11 @@
+// Notes on the course Typescript for Node.js from EPAM UpSkillMe Node.js program
+// Completed by Arkadii Semenov on 2023-03-25
+
+(() => {
+  let input = "...";
+  try {
+    let obj = JSON.parse(input);
+  } catch {
+    console.log("bad input");
+  }
+})();
