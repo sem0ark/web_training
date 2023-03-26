de87856ec4f38dc735accd976ca05d38ee3d21b0
sem0ark <arcadii.sem@gmail.com>
2023-03-26 19:01:48 +0200

    Add settings

--- node/UpSkillMe_NodeJS/023-typescript-with-node/course_files/index.ts
+++ node/UpSkillMe_NodeJS/023-typescript-with-node/course_files/index.ts
@@ -6,15 +6,17 @@ import routes from "./src/routes/crmRoutes";
 import Messenger from "./src/controllers/createMessage";
 
 const app = express();
-const PORT: number = 3000;
 
-const database: string = "mongodb://localhost:27017/tscrm";
+export enum Settings {
+  port = 3000,
+  databaseUrl = "mongodb://localhost:27017/tscrm",
+}
 
-let messenger = new Messenger(PORT);
+let messenger = new Messenger(Settings.port);
 
 // mongoose connection
 // mongoose.Promise = global.Promise; // don't need to use in that course
-mongoose.connect(database, {
+mongoose.connect(Settings.databaseUrl, {
   // compared to MLab using local DB
   useMongoClient: true,
   // useUnifiedTopology: true,
@@ -32,4 +34,4 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => res.send(messenger.messagePrint()));
 
-app.listen(PORT, () => console.log(messenger.messagePrint()));
+app.listen(Settings.port, () => console.log(messenger.messagePrint()));
